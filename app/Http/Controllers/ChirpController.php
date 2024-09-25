56fe0ca747a4ae92875cba394fe37afc9adc53e8
matthew.hand124@gmail.com <74497505+HandMatt@users.noreply.github.com>
2024-09-25 17:22:06 +0100

    add chirp creation and storage

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -10,7 +10,6 @@ class ChirpController extends Controller
      * Display a listing of the resource.
      */
     public function index(): View
-
     {
         return view('chirps', [
             //
